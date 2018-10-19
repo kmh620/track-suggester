ebc99d1d94ae743a8773078e1f236f4ee348d6a5
Student <example@example.com>
2018-10-19 09:36:02 -0700

    Add initial js functions, submit button

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+  $("#questions").submit(function(event) {
+    var location = $("input:radio[name=location]:checked").val();
+    var workstyle = $("input:radio[name:work-style]:checked").val();
+    var development = $("input:radio[name=development]:checked").val();
+    var companySize = $("input:radio[name=company-size]:checked").val();
+    var content = $("input:radio[name=content]:checked").val();
+    
+    event.preventDefault();
+  });
+});
