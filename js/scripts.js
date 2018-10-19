553408638546a2e91e7a46e01cb98b5a3f5e4b29
Student <example@example.com>
2018-10-19 10:35:22 -0700

    Add if else js, debugg

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 
 $(document).ready(function() {
-  $("#questions").submit(function(event) {
+  $("form#questions").submit(function(event) {
+    var workstyle = $("input:radio[name=work-style]:checked").val();
     var location = $("input:radio[name=location]:checked").val();
-    var workstyle = $("input:radio[name:work-style]:checked").val();
     var development = $("input:radio[name=development]:checked").val();
     var companySize = $("input:radio[name=company-size]:checked").val();
     var content = $("input:radio[name=content]:checked").val();
 
-    
+    if (location = portland || seattle) {
+      $("#ruby").show();
+
+    } else {
+      $("#no-track").show();
+    }
 
 
 
