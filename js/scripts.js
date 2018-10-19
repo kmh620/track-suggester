204336e5dddf0821caca7843772665c0a584cc48
Student <example@example.com>
2018-10-19 12:43:51 -0700

    Add answer modal

--- js/scripts.js
+++ js/scripts.js
@@ -10,30 +10,35 @@ $(document).ready(function() {
 
     if (workstyle ==="computer-no" || location ==="neither") {
       $("#no-track").show();
+      $("#suggestion-modal").show();
       $("#php").hide();
       $("#c-sharp").hide();
       $("#ruby").hide();
 
     } else if (development ==="back-end" && content ==="content-mgmt") {
       $("#php").show();
+      $("#suggestion-modal").show();
       $("#no-track").hide();
       $("#c-sharp").hide();
       $("#ruby").hide();
 
     } else if (companySize ==="large" && content ==="business") {
       $("#c-sharp").show();
+      $("#suggestion-modal").show();
       $("#php").hide();
       $("#no-track").hide();
       $("#ruby").hide();
 
     } else if (development ==="front-end" && companySize ==="start-up" && content ==="interactive") {
       $("#ruby").show();
+      $("#suggestion-modal").show();
       $("#php").hide();
       $("#c-sharp").hide();
       $("#no-track").hide();
 
     } else {
       $("#no-track").show();
+      $("#suggestion-modal").show();
       $("#php").hide();
       $("#c-sharp").hide();
       $("#ruby").hide();
